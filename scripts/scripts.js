3949fec1386cceb4ea97b387ae05ed67191545fd
Liam McColgan <l.mccolgan.connect@gmail.com>
2018-01-26 00:15:11 +0000

    26/01/18 commit

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,29 +1,22 @@
 $(document).ready(function(){
-
   // Smooth Scrolling
   var scrollLink = $('.scroll');
-
   scrollLink.click(function(e){
     e.preventDefault();
     $('body,html').animate({
       scrollTop: $(this.hash).offset().top - $('.header-container').outerHeight()
     }, 1500);
   });
-
   // Nav-Icon Animation
   $('#nav-icon').click(function(){
     $('.site-nav').toggleClass('site-nav--open', 400);
     $(this).toggleClass('open');
-    //$('.showcase, .our-approach, .about-us, .leadership, .disc, .sales-management, .mentoring, .estate-agency').toggleClass('blur-filter');
-
   });
-
   // Section Fade Outs
   $(document).on("scroll", function(){
     var pageTop = $(document).scrollTop();
     var pageBottom = pageTop + $(window).height();
     var tags = $(".fade-section");
-
     for (var i = 0; i < tags.length; i++){
       var tag = tags[i];
       if ($(tag).position().top < pageBottom - 200){
@@ -33,16 +26,13 @@ $(document).ready(function(){
       }
     }
   });
-
   //Parallax 
   function parallax(){
     var wScroll = $(window).scrollTop();
     $('.showcase-outer').css('top',(wScroll * 0.04)+'em');
     $('.frame').css('top',(wScroll * 0.02)+'em');
     $('.hand').css('top',(wScroll * 0.03)+'em');
-    
   }
-
   // Link Switching
   function linkSwitching(){
     var scrollBarLocation = $(this).scrollTop();
@@ -54,7 +44,6 @@ $(document).ready(function(){
       }
     });
   }
-
   // Element Fade Outs
   function fadeOut(){
     
@@ -66,7 +55,6 @@ $(document).ready(function(){
       linkSwitching();
       fadeOut();
   });
-
   // Modal Variables
   var modal1 = document.getElementById('kingsley-modal');
   var modal2 = document.getElementById('nicholas-modal');
@@ -74,7 +62,6 @@ $(document).ready(function(){
   var btn2 = document.getElementById('nicholasBio');
   var close1 = document.getElementsByClassName("close1")[0];
   var close2 = document.getElementsByClassName("close2")[0];
-
   // Modal Functions
   btn1.onclick = function(){
     modal1.style.display = "block";
