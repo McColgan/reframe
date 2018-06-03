92821b73d0c26b8ed869e0c8f2c3412fb72fe6f4
Liam McColgan <l.mccolgan.connect@gmail.com>
2018-06-03 12:45:18 +0100

    Foxed some bugs/UI errors + better icons

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -12,26 +12,80 @@ $(document).ready(function(){
     $('.site-nav').toggleClass('site-nav--open', 400);
     $(this).toggleClass('open');
   });
-  // Section Fade Outs
-  $(document).on("scroll", function(){
-    var pageTop = $(document).scrollTop();
-    var pageBottom = pageTop + $(window).height();
-    var tags = $(".fade-section");
-    for (var i = 0; i < tags.length; i++){
-      var tag = tags[i];
-      if ($(tag).position().top < pageBottom - 200){
-        $(tag).addClass("visible");
-      } else {
-        $(tag).removeClass("visible");
-      }
-    }
-  });
+  
+  
   //Parallax 
   function parallax(){
     var wScroll = $(window).scrollTop();
     $('.showcase-outer').css('top',(wScroll * 0.04)+'em');
     $('.frame').css('top',(wScroll * 0.02)+'em');
     $('.hand').css('top',(wScroll * 0.03)+'em');
+  
+    // Our-approach fade-ins
+    if(wScroll > $('.our-approach').offset().top - ($(window).height() / 1.3)){
+      $('.our-approach-header').addClass('is-showing');
+      $('.our-approach-h1').addClass('is-showing');
+      $('.our-approach-p').addClass('is-showing');
+    };
+     // About-us fade-ins
+    if(wScroll > $('.about-us').offset().top - ($(window).height() / 1.3)){
+      $('.about-us-h1').addClass('is-showing');
+      $('.box-item').addClass('is-showing');
+    };
+     // Leadership fade-ins
+    if(wScroll > $('.leadership').offset().top - ($(window).height() / 1.3)){
+      $('.section-h1').addClass('is-showing');
+      $('.leadership-container').addClass('is-showing');
+    };
+     // Disc fade-ins
+    if(wScroll > $('.disc').offset().top - ($(window).height() / 1.3)){
+      $('.disc-header').addClass('is-showing');
+      $('.disc-p1').addClass('is-showing');
+      $('.disc-flexbox').addClass('is-showing');
+      $('.disc-flexbox-2').addClass('is-showing');
+    };
+     // Sales-management fade-ins
+    if(wScroll > $('.sales-management').offset().top - ($(window).height() / 1.3)){
+      $('.sales-h1').addClass('is-showing');
+      $('.sales-p1').addClass('is-showing');
+    };
+    if(wScroll > $('.sales-flexbox').offset().top - ($(window).height() / 1.2)){
+      $('.sales-h1').addClass('is-showing');
+      $('.sales-p1').addClass('is-showing');
+      $('.sales-box').each(function(i){
+        setTimeout(function(){
+            $('.sales-box').eq(i).addClass('is-showing');
+        }, 250 * (i+1));
+        });
+    };
+     // mentoring fade-ins
+    if(wScroll > $('.mentoring').offset().top - ($(window).height() / 1.1)){
+      $('.mentoring-h1').addClass('is-showing');
+      $('.mentoring-p1').addClass('is-showing');
+      $('.mentoring-box').each(function(i){
+        setTimeout(function(){
+            $('.mentoring-box').eq(i).addClass('is-showing');
+        }, 250 * (i+1));
+        });
+    };
+     // Estate-agency fade-ins
+    if(wScroll > $('.estate-agency').offset().top - ($(window).height() / 1.3)){
+      $('.estate-outer1 h1').addClass('is-showing');
+      $('.estate-h1').addClass('is-showing');
+      $('.estate-img').addClass('is-showing');
+    };
+    if(wScroll > $('.estate-section2').offset().top - ($(window).height() / 1.3)){
+      $('.estate-section2 video').addClass('is-showing');
+      $('.estate2-h1').addClass('is-showing');
+      $('.estate-outer2 p').addClass('is-showing');
+    };
+
+    if(wScroll > $('.estate-section1').offset().top - $(window).height()){
+      $('.estate-section1-bg').css('top',(wScroll * 0.2 )+'px');
+    };
+    if(wScroll > $('.estate-section2').offset().top - $(window).height()){
+      $('.estate-section2-bg').css('top',(wScroll * 0.2 )+'px');
+    }
   }
   // Link Switching
   function linkSwitching(){
