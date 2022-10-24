586d73f7ec238360bc90d1a60bc67cf62de95e29
Liam McColgan <l.mccolgan.connect@email.com>
2022-10-24 17:28:26 +0100

    reset skeleton DOM and added npm

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -15,78 +15,50 @@ $(document).ready(function(){
   
   
   //Parallax 
-  function parallax(){
-    var wScroll = $(window).scrollTop();
-    $('.showcase-outer').css('top',(wScroll * 0.04)+'em');
-    $('.frame').css('top',(wScroll * 0.02)+'em');
-    $('.hand').css('top',(wScroll * 0.03)+'em');
+  //function parallax(){
+    //var wScroll = $(window).scrollTop();
+    //$('.showcase-outer').css('top',(wScroll * 0.04)+'em');
+    //$('.frame').css('top',(wScroll * 0.02)+'em');
+    //$('.hand').css('top',(wScroll * 0.03)+'em');
   
     // Our-approach fade-ins
-    if(wScroll > $('.our-approach').offset().top - ($(window).height() / 1.3)){
-      $('.our-approach-header').addClass('is-showing');
-      $('.our-approach-h1').addClass('is-showing');
-      $('.our-approach-p').addClass('is-showing');
-    };
+   // if(wScroll > $('.our-approach').offset().top - ($(window).height() / 1.3)){
+   //   $('.our-approach-header').addClass('is-showing');
+   //   $('.our-approach-h1').addClass('is-showing');
+   //   $('.our-approach-p').addClass('is-showing');
+   // };
      // About-us fade-ins
-    if(wScroll > $('.about-us').offset().top - ($(window).height() / 1.3)){
-      $('.about-us-h1').addClass('is-showing');
-      $('.box-item').addClass('is-showing');
-    };
+   // if(wScroll > $('.about-us').offset().top - ($(window).height() / 1.3)){
+   //   $('.about-us-h1').addClass('is-showing');
+   //   $('.box-item').addClass('is-showing');
+   // };
      // Leadership fade-ins
-    if(wScroll > $('.leadership').offset().top - ($(window).height() / 1.3)){
-      $('.section-h1').addClass('is-showing');
-      $('.leadership-container').addClass('is-showing');
-    };
+    //if(wScroll > $('.leadership').offset().top - ($(window).height() / 1.3)){
+      //$('.section-h1').addClass('is-showing');
+      //$('.leadership-container').addClass('is-showing');
+    //};
      // Disc fade-ins
-    if(wScroll > $('.disc').offset().top - ($(window).height() / 1.3)){
-      $('.disc-header').addClass('is-showing');
-      $('.disc-p1').addClass('is-showing');
-      $('.disc-flexbox').addClass('is-showing');
-      $('.disc-flexbox-2').addClass('is-showing');
-    };
+    //if(wScroll > $('.disc').offset().top - ($(window).height() / 1.3)){
+      //$('.disc-header').addClass('is-showing');
+      //$('.disc-p1').addClass('is-showing');
+      //$('.disc-flexbox').addClass('is-showing');
+      //$('.disc-flexbox-2').addClass('is-showing');
+    //};
      // Sales-management fade-ins
-    if(wScroll > $('.sales-management').offset().top - ($(window).height() / 1.3)){
-      $('.sales-h1').addClass('is-showing');
-      $('.sales-p1').addClass('is-showing');
-    };
-    if(wScroll > $('.sales-flexbox').offset().top - ($(window).height() / 1.2)){
-      $('.sales-h1').addClass('is-showing');
-      $('.sales-p1').addClass('is-showing');
-      $('.sales-box').each(function(i){
-        setTimeout(function(){
-            $('.sales-box').eq(i).addClass('is-showing');
-        }, 250 * (i+1));
-        });
-    };
-     // mentoring fade-ins
-    if(wScroll > $('.mentoring').offset().top - ($(window).height() / 1.1)){
-      $('.mentoring-h1').addClass('is-showing');
-      $('.mentoring-p1').addClass('is-showing');
-      $('.mentoring-box').each(function(i){
-        setTimeout(function(){
-            $('.mentoring-box').eq(i).addClass('is-showing');
-        }, 250 * (i+1));
-        });
-    };
-     // Estate-agency fade-ins
-    if(wScroll > $('.estate-agency').offset().top - ($(window).height() / 1.3)){
-      $('.estate-outer1 h1').addClass('is-showing');
-      $('.estate-h1').addClass('is-showing');
-      $('.estate-img').addClass('is-showing');
-    };
-    if(wScroll > $('.estate-section2').offset().top - ($(window).height() / 1.3)){
-      $('.estate-section2 video').addClass('is-showing');
-      $('.estate2-h1').addClass('is-showing');
-      $('.estate-outer2 p').addClass('is-showing');
-    };
-
-    if(wScroll > $('.estate-section1').offset().top - $(window).height()){
-      $('.estate-section1-bg').css('top',(wScroll * 0.2 )+'px');
-    };
-    if(wScroll > $('.estate-section2').offset().top - $(window).height()){
-      $('.estate-section2-bg').css('top',(wScroll * 0.2 )+'px');
-    }
-  }
+    //if(wScroll > $('.sales-management').offset().top - ($(window).height() / 1.3)){
+      //$('.sales-h1').addClass('is-showing');
+      //$('.sales-p1').addClass('is-showing');
+    //};
+    //if(wScroll > $('.sales-flexbox').offset().top - ($(window).height() / 1.2)){
+      //$('.sales-h1').addClass('is-showing');
+      //$('.sales-p1').addClass('is-showing');
+      //$('.sales-box').each(function(i){
+        //setTimeout(function(){
+            //$('.sales-box').eq(i).addClass('is-showing');
+        //}, 250 * (i+1));
+        //});
+    //};
+    
   // Link Switching
   function linkSwitching(){
     var scrollBarLocation = $(this).scrollTop();
@@ -109,32 +81,5 @@ $(document).ready(function(){
       linkSwitching();
       fadeOut();
   });
-  // Modal Variables
-  var modal1 = document.getElementById('kingsley-modal');
-  var modal2 = document.getElementById('nicholas-modal');
-  var btn1 = document.getElementById('kingsleyBio');
-  var btn2 = document.getElementById('nicholasBio');
-  var close1 = document.getElementsByClassName("close1")[0];
-  var close2 = document.getElementsByClassName("close2")[0];
-  // Modal Functions
-  btn1.onclick = function(){
-    modal1.style.display = "block";
-  };
-  btn2.onclick = function(){
-    modal2.style.display = "block";
-  };
-  close1.onclick = function(){
-    modal1.style.display = "none";
-  };
-  close2.onclick = function(){
-    modal2.style.display = "none";
-  };
-  window.onclick =  function(event){
-    if (event.target == modal1){
-      modal1.style.display = "none";
-
-    } else if (event.target == modal2){
-      modal2.style.display = "none";
-    }
-  };
+  
 });
